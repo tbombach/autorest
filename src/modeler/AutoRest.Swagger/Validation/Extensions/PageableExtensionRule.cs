8c1e862c17381c914ae242489820a48c32c62168
Thomas Bombach <bombach.thomas@gmail.com>
2016-08-30 10:11:52 -0700

    Adding missing using

--- src/modeler/AutoRest.Swagger/Validation/Extensions/PageableExtensionRule.cs
+++ src/modeler/AutoRest.Swagger/Validation/Extensions/PageableExtensionRule.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using AutoRest.Core.Logging;
 using AutoRest.Core.Validation;
 using AutoRest.Swagger.Model;
 using System.Collections.Generic;
